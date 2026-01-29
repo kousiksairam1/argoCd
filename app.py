28af82b6a83efaa9cf0ef1c78f8d9967713cfb80
kousiksairam1 <52517267+kousiksairam1@users.noreply.github.com>
2026-01-29 16:18:45 +0530

    Update greeting message in home route

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from GitHub Actions Azure CI/CD Pipeline!"
+    return "Hello from GitHub Actions Azure And Agro CI/CD Pipeline!"
 
 if __name__ == "__main__":
     # Bind to 0.0.0.0 so it is reachable from outside the container
